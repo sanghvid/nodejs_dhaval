5a42de71c8b1bb30f52d2c618c627abd38f77fc6
Dhaval Sanghvi <sanghvid@pmc.local>
2019-04-20 14:10:00 +0530

    json get applied

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get('/',(req,res,next)=>{
     res.send('hello dhaval here!!')
 })
 
+app.get('/json',(req,res,next)=>{
+
+    const data={
+        "hello":"This is Dhaval Sanghvi"
+        }
+        res.json(data)
+})
+
 app.listen(3000)
